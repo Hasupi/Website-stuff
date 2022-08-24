415b4f4dde6befe448107141b38f7c57267e3dc5
Hasupi <ryogawisjnuery@gmail.com>
2022-08-24 20:37:35 +1200

    Minor changes
    Changed some stuff in css to improve it a bit. Adjusted code to fit pep8 formatting.

--- main.py
+++ main.py
@@ -6,15 +6,18 @@ app = Flask(__name__)
 connection = sqlite3.connect('arcaea_attempt2.db', check_same_thread=False)
 cur = connection.cursor()
 
+
 @app.route("/")
 def home():
     cur.execute("""SELECT *, (CASE WHEN FTR = '9+' THEN 9.5 WHEN FTR = '10+' THEN 10.5 ELSE FTR END) AS sort FROM Song ORDER BY sort DESC""")
     Songs = cur.fetchall()
     return render_template("index.html", Songs=Songs)
 
+
 @app.route("/about")
 def about():
     return render_template("about.html")
+
 
 @app.route("/packentry/<int:id>")
 def packentry(id):
@@ -24,12 +27,13 @@ def packentry(id):
     songs = cur.fetchall()
     return render_template("packentry.html", pack = packentry, songs=songs)
 
+
 @app.route("/packs")
 def packs():
     cur.execute("SELECT * FROM Packs")
     packs = cur.fetchall()
-    print(packs)
     return render_template("packs.html", packs=packs)
+
 
 @app.route("/songs/<int:id>")
 def songs(id):
@@ -37,9 +41,11 @@ def songs(id):
     song = cur.fetchone()
     return render_template("song.html", song=song)
     
-@app.errorhandler(404)
+
+@app.errorhandler(404)  
 def die(e):
     return render_template("404.html"), 404
+
 
 if __name__ == "__main__":
     app.run(debug="true", host="0.0.0.0", port="8000")
