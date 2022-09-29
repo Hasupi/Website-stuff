29a149d2b1a91a5a5e6312627a8c5798ba0edbce
Hasupi <ryogawisjnuery@gmail.com>
2022-09-30 05:59:01 +1300

    Added a bunch more images

--- main.py
+++ main.py
@@ -8,18 +8,26 @@ cur = connection.cursor()
 
 
 @app.route("/")
+
+
 def home():
-    cur.execute("""SELECT *, (CASE WHEN FTR = '9+' THEN 9.5 WHEN FTR = '10+' THEN 10.5 ELSE FTR END) AS sort FROM Song ORDER BY sort DESC""")
+    cur.execute("""SELECT *, (CASE WHEN FTR = '9+' THEN 9.5 WHEN FTR = '10+' 
+                    THEN 10.5 ELSE FTR END) AS sort FROM Song 
+                    ORDER BY sort DESC""")
     Songs = cur.fetchall()
     return render_template("index.html", Songs=Songs)
 
 
 @app.route("/about")
+
+
 def about():
     return render_template("about.html")
 
 
 @app.route("/packentry/<int:id>")
+
+
 def packentry(id):
     cur.execute("SELECT * FROM Packs WHERE id = ?", (id,))
     packentry = cur.fetchone()
@@ -29,6 +37,8 @@ def packentry(id):
 
 
 @app.route("/packs")
+
+
 def packs():
     cur.execute("""SELECT Packs.id,
                         pack_name,
@@ -40,18 +50,20 @@ def packs():
 
 
 @app.route("/songs/<int:id>")
+
+
 def songs(id):
     cur.execute("SELECT * FROM Song WHERE id = ?", (id,))
     song = cur.fetchone()
     return render_template("song.html", song=song)
     
 
-@app.errorhandler(404)  
+@app.errorhandler(404)
+
+
 def die(e):
     return render_template("404.html"), 404
 
 
 if __name__ == "__main__":
     app.run(debug="true", host="0.0.0.0", port="8000")
-    
-    
